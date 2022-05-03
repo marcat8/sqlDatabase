ad5b5f6297d934c9bf6a04a8f19fe6564e7fcfc5
marcat8 <marcat90@gmail.com>
2022-05-03 12:16:10 +0200

    query 1

--- EserciziAnagrafe.sql
+++ EserciziAnagrafe.sql
@@ -8,4 +8,10 @@ select Persona.Nome, Città.Regione
 from Persona, Città
 where Persona.CittàNascita = Città.Nome && Persona.Età >= 18
 
+/*Query 2*/
+
+select PG.Nome, PF.Nome
+from Genia join Persona PG on Genia.Genitore=PG.Nome
+  join Persona PF on Genia.Figlio=PF.Nome
+where PG.Città = PF.Città
 
