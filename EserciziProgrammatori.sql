c300238122ebd7f0450ba07d749069a6729e1f9a
marcat8 <marcat90@gmail.com>
2022-05-03 12:25:21 +0200

    query 2

--- EserciziProgrammatori.sql
+++ EserciziProgrammatori.sql
@@ -3,3 +3,10 @@ select programmatore.Codice
 from autore  join programma on programma.id = Autore.id
     join programmatore on programmatore.codice = autore.codice
 where programma.anno >= 2000 && programma.linguaggio = "Java";
+
+/*query 2*/
+select programmatore.Nome, programmatore.Categoria
+from autore  join programma on programma.id = Autore.id
+    join programmatore on programmatore.codice = autore.codice
+where programma.linguaggio != "Python"
+order by programmatore.Nome
