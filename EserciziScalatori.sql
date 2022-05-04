6dd01c00c66bf4ec4a76b071ad4de2a7686d2e35
marcat8 <marcat90@gmail.com>
2022-05-04 12:49:24 +0200

    query 3

--- EserciziScalatori.sql
+++ EserciziScalatori.sql
@@ -8,3 +8,10 @@ from scalata join nazione on scalata.nazione = nazione.nome
     join scalatore on scalata.scalatore = scalatore.cf
 where scalatore.annoNascita < 1980
 order by scalatore.cf, nazione.continente
+
+/*query 3*/
+select distinct nazione.nome, nazione.continente
+from scalata join nazione on scalata.nazione = nazione.nome
+    join scalatore on scalata.scalatore = scalatore.cf
+where (scalata.anno - scalatore.annoNascita) <=20
+
