2a3635814b93e8ee19ae5c4e3b144f7bd9e70b38
marcat8 <marcat90@gmail.com>
2022-05-04 12:35:40 +0200

    query 2

--- EserciziScalatori.sql
+++ EserciziScalatori.sql
@@ -1,3 +1,10 @@
 /*query 1*/
 select scalata.scalatore as A, nazione.continente as B
 from scalata join nazione on scalata.nazione = nazione.nome
+
+/*query 2*/
+select distinct scalata.scalatore, nazione.continente
+from scalata join nazione on scalata.nazione = nazione.nome
+    join scalatore on scalata.scalatore = scalatore.cf
+where scalatore.annoNascita < 1980
+order by scalatore.cf, nazione.continente
