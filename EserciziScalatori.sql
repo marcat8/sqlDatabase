f38cdd9c584daeb7df22ec32a2fd20747af0f7e4
marcat8 <marcat90@gmail.com>
2022-05-05 14:47:40 +0200

    query 6,7

--- EserciziScalatori.sql
+++ EserciziScalatori.sql
@@ -15,3 +15,25 @@ from scalata join nazione on scalata.nazione = nazione.nome
     join scalatore on scalata.scalatore = scalatore.cf
 where (scalata.anno - scalatore.annoNascita) <=20
 
+/*query 4*/
+select nazione.nome, count(scalata.nazione) as numero
+from scalata join nazione on scalata.nazione = nazione.nome
+    join scalatore on scalata.scalatore = scalatore.cf
+where scalatore.nazioneNascita = scalata.nazione
+group by nazione.nome
+
+/*query 5*/
+
+/*query 6*/
+select CF, nazioneNascita, continente, nazione
+from scalatore left join scalata on CF = scalatore
+    join Nazione on nome = nazioneNascita
+where continente != "America"
+
+/*query 7*/
+select continente, nazione, anno, count(*)
+from nazione join scalata on nazione.nome = scalata.nazione
+group by scalata,nazione, scalata.anno
+having count(*) > 1
+order by nazione.continente, scalata.anno
+
